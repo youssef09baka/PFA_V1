08f7f55a99dcaf21fe533c8bba44898c4d56ec87
youssef09baka <youssef09baka@gmail.com>
2026-04-02 21:16:21 +0100

    add trend controller

---
+++ backend/controllers/trendController.js
@@ -0,0 +1,31 @@
+const getTrends = async (req, res) => {
+  try {
+    const trends = [
+      {
+        title: "AI tools for students",
+        popularity: 85,
+        growth: 20,
+        source: "reddit"
+      },
+      {
+        title: "Fitness at home",
+        popularity: 70,
+        growth: 15,
+        source: "google trends"
+      },
+      {
+        title: "Online business ideas 2026",
+        popularity: 90,
+        growth: 25,
+        source: "youtube"
+      }
+    ];
+
+    res.status(200).json(trends);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getTrends };
